2306b3996d6f39325e4317b862b75ff05518b588
Olesia Yurchenko <olesya.yurchenko91@gmail.com>
2021-12-13 16:55:31 +0200

    bugfix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,14 +1,13 @@
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = `feedback-form-state`;
-const formData = {};
 
 const form = document.querySelector(`.feedback-form`);
 const email = document.querySelector(`.feedback-form input`);
 const message = document.querySelector(`.feedback-form textarea`);
 
-form.addEventListener(`submit`, onFormSubmit);
 form.addEventListener(`input`, throttle(onTextareaInput, 500));
+form.addEventListener(`submit`, onFormSubmit);
 
 populateTextarea();
 
@@ -17,15 +16,15 @@ function onFormSubmit(evt) {
 
     console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
 
-    evt.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KEY);
-    
+    form.reset();
+    localStorage.removeItem(STORAGE_KEY);    
  }
 
 function onTextareaInput(evt) {
+    const formData = {...JSON.parse(localStorage.getItem(STORAGE_KEY))};
     formData[evt.target.name] = evt.target.value;
     const formDataJSON = JSON.stringify(formData);
-
+      
     localStorage.setItem(STORAGE_KEY, formDataJSON);
 }
 
